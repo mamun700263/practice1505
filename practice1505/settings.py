06fb029dc92860b06f60cff877ca50c8f3f6a02e
mamun700263 <mamun700263@gmail.com>
2024-06-28 13:59:33 +0600

    may work now

--- practice1505/settings.py
+++ practice1505/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'album',
+    'musician',
+    'crispy',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
